021a2c0536869f25abdd34c922b01d3a3790ab57
Dimitur Botev <botev_dimitur@abv.bg>
2016-06-10 21:07:21 +0300

    JBrowse

---
+++ JustBrowse.Designer.cs
@@ -0,0 +1,171 @@
+﻿namespace JustBrowse
+{
+    partial class JustBrowse
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(JustBrowse));
+            this.URLInput = new System.Windows.Forms.TextBox();
+            this.buttonTip = new System.Windows.Forms.ToolTip(this.components);
+            this.makeHomePage = new System.Windows.Forms.Button();
+            this.homePage = new System.Windows.Forms.Button();
+            this.refresh = new System.Windows.Forms.Button();
+            this.forward = new System.Windows.Forms.Button();
+            this.back = new System.Windows.Forms.Button();
+            this.tabControl = new System.Windows.Forms.TabControl();
+            this.progressBar = new System.Windows.Forms.ProgressBar();
+            this.SuspendLayout();
+            // 
+            // URLInput
+            // 
+            this.URLInput.Anchor = ((System.Windows.Forms.AnchorStyles)(((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.URLInput.AutoCompleteMode = System.Windows.Forms.AutoCompleteMode.Suggest;
+            this.URLInput.AutoCompleteSource = System.Windows.Forms.AutoCompleteSource.RecentlyUsedList;
+            this.URLInput.Font = new System.Drawing.Font("Microsoft Sans Serif", 10F, System.Drawing.FontStyle.Italic);
+            this.URLInput.Location = new System.Drawing.Point(162, 3);
+            this.URLInput.Name = "URLInput";
+            this.URLInput.Size = new System.Drawing.Size(415, 23);
+            this.URLInput.TabIndex = 6;
+            this.URLInput.Text = "http://";
+            this.URLInput.KeyDown += new System.Windows.Forms.KeyEventHandler(this.URLInput_KeyDown);
+            // 
+            // makeHomePage
+            // 
+            this.makeHomePage.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.makeHomePage.Image = global::JustBrowse.Properties.Resources.newHopePage;
+            this.makeHomePage.Location = new System.Drawing.Point(611, 3);
+            this.makeHomePage.Name = "makeHomePage";
+            this.makeHomePage.Size = new System.Drawing.Size(34, 23);
+            this.makeHomePage.TabIndex = 7;
+            this.buttonTip.SetToolTip(this.makeHomePage, "Make this page default home page");
+            this.makeHomePage.UseVisualStyleBackColor = true;
+            this.makeHomePage.Click += new System.EventHandler(this.makeHomePage_Click);
+            // 
+            // homePage
+            // 
+            this.homePage.Image = global::JustBrowse.Properties.Resources.home_512;
+            this.homePage.Location = new System.Drawing.Point(122, 3);
+            this.homePage.Name = "homePage";
+            this.homePage.Size = new System.Drawing.Size(34, 23);
+            this.homePage.TabIndex = 4;
+            this.buttonTip.SetToolTip(this.homePage, "Go to home page");
+            this.homePage.UseVisualStyleBackColor = true;
+            this.homePage.Click += new System.EventHandler(this.homePage_Click);
+            // 
+            // refresh
+            // 
+            this.refresh.Image = global::JustBrowse.Properties.Resources.refresh;
+            this.refresh.Location = new System.Drawing.Point(82, 3);
+            this.refresh.Name = "refresh";
+            this.refresh.Size = new System.Drawing.Size(34, 23);
+            this.refresh.TabIndex = 3;
+            this.buttonTip.SetToolTip(this.refresh, "Refresh this page");
+            this.refresh.UseVisualStyleBackColor = true;
+            this.refresh.Click += new System.EventHandler(this.refresh_Click);
+            // 
+            // forward
+            // 
+            this.forward.ForeColor = System.Drawing.SystemColors.ControlLight;
+            this.forward.Image = global::JustBrowse.Properties.Resources.forward;
+            this.forward.Location = new System.Drawing.Point(42, 3);
+            this.forward.Name = "forward";
+            this.forward.Size = new System.Drawing.Size(34, 23);
+            this.forward.TabIndex = 2;
+            this.buttonTip.SetToolTip(this.forward, "Go to the next page");
+            this.forward.UseVisualStyleBackColor = true;
+            this.forward.Click += new System.EventHandler(this.forward_Click);
+            // 
+            // back
+            // 
+            this.back.Image = global::JustBrowse.Properties.Resources.back1;
+            this.back.Location = new System.Drawing.Point(2, 3);
+            this.back.Name = "back";
+            this.back.Size = new System.Drawing.Size(34, 23);
+            this.back.TabIndex = 1;
+            this.buttonTip.SetToolTip(this.back, "Go to the previus page");
+            this.back.UseVisualStyleBackColor = true;
+            this.back.Click += new System.EventHandler(this.back_Click);
+            // 
+            // tabControl
+            // 
+            this.tabControl.Anchor = ((System.Windows.Forms.AnchorStyles)((((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Bottom) 
+            | System.Windows.Forms.AnchorStyles.Left) 
+            | System.Windows.Forms.AnchorStyles.Right)));
+            this.tabControl.Location = new System.Drawing.Point(2, 32);
+            this.tabControl.Name = "tabControl";
+            this.tabControl.SelectedIndex = 0;
+            this.tabControl.Size = new System.Drawing.Size(717, 335);
+            this.tabControl.TabIndex = 9;
+            this.tabControl.MouseClick += new System.Windows.Forms.MouseEventHandler(this.tabControl_MouseClick);
+            // 
+            // progressBar
+            // 
+            this.progressBar.Anchor = ((System.Windows.Forms.AnchorStyles)((System.Windows.Forms.AnchorStyles.Top | System.Windows.Forms.AnchorStyles.Right)));
+            this.progressBar.ForeColor = System.Drawing.Color.FromArgb(((int)(((byte)(0)))), ((int)(((byte)(192)))), ((int)(((byte)(192)))));
+            this.progressBar.Location = new System.Drawing.Point(581, 3);
+            this.progressBar.Name = "progressBar";
+            this.progressBar.Size = new System.Drawing.Size(24, 23);
+            this.progressBar.TabIndex = 12;
+            // 
+            // JustBrowse
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(716, 367);
+            this.Controls.Add(this.progressBar);
+            this.Controls.Add(this.tabControl);
+            this.Controls.Add(this.makeHomePage);
+            this.Controls.Add(this.URLInput);
+            this.Controls.Add(this.homePage);
+            this.Controls.Add(this.refresh);
+            this.Controls.Add(this.forward);
+            this.Controls.Add(this.back);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "JustBrowse";
+            this.Text = "JBrowse";
+            this.Load += new System.EventHandler(this.JustBrowse_Load);
+            this.MouseClick += new System.Windows.Forms.MouseEventHandler(this.JustBrowse_MouseClick);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button back;
+        private System.Windows.Forms.Button forward;
+        private System.Windows.Forms.Button refresh;
+        private System.Windows.Forms.Button homePage;
+        private System.Windows.Forms.TextBox URLInput;
+        private System.Windows.Forms.Button makeHomePage;
+        private System.Windows.Forms.ToolTip buttonTip;
+        private System.Windows.Forms.TabControl tabControl;
+        private System.Windows.Forms.ProgressBar progressBar;
+    }
+}
+
---
+++ JustBrowse.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Text.RegularExpressions;
+
+namespace JustBrowse
+{
+    public partial class JustBrowse : Form
+    {
+        private string _defaulURL = "http://www.google.com";
+        private WebBrowser _webBrowser = new WebBrowser();
+        private int _tabIndex = 0;
+        private string _nextTabURL = "http://";
+
+        public JustBrowse()
+        {
+            InitializeComponent();
+            
+            this.URLInput.Text = this._defaulURL.ToString();
+        }
+
+        public static string ReturURLInProperFormat(string input)
+        {
+
+            //proper format http://www.Somedomain
+            string pattern = @"http://www.";
+            Regex regex = new Regex(pattern);
+            if (regex.IsMatch(input))
+            {
+                return input;
+            } else
+            {
+                //check for other options
+                //missing http:// part
+                regex = new Regex(@"http://");
+                if (regex.IsMatch(input))
+                {
+                    //http fragmet is not missing
+                    regex = new Regex(@"www.");
+                    if (regex.IsMatch(input))
+                    {
+                        //http://www. fragmet both are not missing -> wrong URL
+                        return input;
+                    }
+                    else
+                    {
+                        //Issue Need to be fixed !
+                        //www. part is missing
+                        return String.Format("{0}www.{1}",String.Format("http://"), input);
+                    }
+                } else
+                {
+                    //check if www. is missing
+                    regex = new Regex(@"www.");
+                    if (regex.IsMatch(input))
+                    {
+                        //only http:// is missing
+                        return String.Format("http://{0}", input);
+                    } else
+                    {
+                        //http and www. fragmets are missing
+                        return String.Format("http://www.{0}", input);
+                    }
+                }
+
+            }
+        }
+
+        private void URLInput_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyData == Keys.Enter)
+            {   
+                e.SuppressKeyPress = true;
+                this.tabControl.SelectTab(this.tabControl.SelectedIndex);
+                ((WebBrowser)tabControl.SelectedTab.Controls[0])
+                .Navigate(ReturURLInProperFormat(URLInput.Text.ToString()));
+                
+
+            }
+        }
+
+        private void back_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl.SelectedTab.Controls[this._tabIndex]).GoBack();      
+        }
+
+        private void forward_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl.SelectedTab.Controls[this._tabIndex]).GoForward();
+        }
+
+        private void refresh_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl.SelectedTab.Controls[0]).Refresh();
+        }
+
+        private void homePage_Click(object sender, EventArgs e)
+        {
+            ((WebBrowser)tabControl.SelectedTab.Controls[0])
+                .Navigate(ReturURLInProperFormat(this._defaulURL));
+        }
+
+        private void makeHomePage_Click(object sender, EventArgs e)
+        {
+            if (URLInput.Text.ToString() != null || URLInput.Text.ToString() != string.Empty)
+            {
+                this._defaulURL = ReturURLInProperFormat(URLInput.Text.ToString());
+            }
+        }
+
+        private void JustBrowse_Load(object sender, EventArgs e)
+        {
+
+            this._webBrowser.ScriptErrorsSuppressed = true;
+            this._webBrowser.Dock = DockStyle.Fill;
+            this._webBrowser.Visible = true;
+            this._webBrowser.DocumentCompleted += _webBrowser_DocumentCompleted;
+            tabControl.TabPages.Add("New Page");
+            tabControl.SelectTab(this._tabIndex);
+            tabControl.SelectedTab.Controls.Add(this._webBrowser);
+            ((WebBrowser)tabControl.SelectedTab.Controls[0]).Navigate(this._defaulURL);
+            
+
+        }
+
+        private void _webBrowser_DocumentCompleted(object sender, WebBrowserDocumentCompletedEventArgs e)
+        {
+            this.tabControl.SelectedTab.Text = ((WebBrowser)tabControl.SelectedTab.Controls[0]).DocumentTitle;
+        }
+
+       
+
+        private void closeTab_Click(object sender, EventArgs e)
+        {
+           
+        }
+
+        private void tabControl_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                ContextMenuStrip closeMenu = new ContextMenuStrip();
+                closeMenu.Items.Add("Close Tab").Name = "close";
+                closeMenu.Items.Add("Add new Tab").Name = "newTab";
+                closeMenu.Show(this.tabControl, new Point(e.X, e.Y));
+                closeMenu.ItemClicked += CloseMenu_ItemClicked;
+            }
+        }
+
+        private void CloseMenu_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if(e.ClickedItem.Name == "close")
+            {
+                if(tabControl.TabPages.Count - 1 > 0)
+                {
+                    tabControl.TabPages.RemoveAt(tabControl.SelectedIndex);
+                    this._tabIndex -= 1;
+                }
+            }
+            if (e.ClickedItem.Name == "newTab")
+            {
+                AddNewTab();
+            }
+        }
+
+        private void JustBrowse_MouseClick(object sender, MouseEventArgs e)
+        {
+            if(e.Button == MouseButtons.Right)
+            {
+                ContextMenuStrip menuStrip = new ContextMenuStrip();
+                menuStrip.Items.Add("Add new Tab").Name = "Add new tab";
+                menuStrip.Show(this, e.X, e.Y);
+                menuStrip.ItemClicked += MenuStrip_ItemClicked;
+            }
+        }
+
+        private void MenuStrip_ItemClicked(object sender, ToolStripItemClickedEventArgs e)
+        {
+            if(e.ClickedItem.Name == "Add new tab")
+            {              
+                AddNewTab();
+            }
+        }
+        private  void AddNewTab()
+        {
+            this._tabIndex = this._tabIndex + 1;
+            this._webBrowser = new WebBrowser();
+            this._webBrowser.ScriptErrorsSuppressed = true;
+            this._webBrowser.Dock = DockStyle.Fill;
+            this._webBrowser.Visible = true;
+            this._webBrowser.DocumentCompleted += _webBrowser_DocumentCompleted;
+            tabControl.TabPages.Add("New Page");
+            tabControl.SelectTab(this._tabIndex);
+            tabControl.SelectedTab.Controls.Add(this._webBrowser);
+            this.URLInput.Text = this._nextTabURL.ToString();
+        }
+           
+
+        
+    }
+}
